491195569f64868168af8619dfd2abd46e6aa46b
София Кокорева <71560782+rainsuncloud@users.noreply.github.com>
2020-09-24 12:42:28 +0300

    Лабораторная сделана

--- Lab4/Lab4/Lab4.cpp
+++ Lab4/Lab4/Lab4.cpp
@@ -32,16 +32,40 @@ int main()
         scanf_s("%f", &b);
         printf("Среднее арифметическое a и b = %.2f", (a + b) / 2);
     }
-    else if (num == 4) {
+else if (num == 4) {
+        float l, k, x, y;
+        printf("x = ");
+        scanf_s("%f", &l);
+        printf("y = ");
+        scanf_s("%f", &k);
+        x = pow(l, 2);
+        y = pow(k, 2);
 
+        if (x == 0 || y == 0) {
+            printf("Требуется ввести два ненулевых числа");
+        }
+        else {
+            printf("x + y = %.2f\nx - y = %.2f\nx * y = %.2f\nx / y = %.2f", x + y, x - y, x * y, x / y);
+        }
     }
     else if (num == 5) {
+        float m, n, s, t;
+        printf("m = ");
+        scanf_s("%f", &m);
+        printf("n = ");
+        scanf_s("%f", &n);
+        s = fabs(m);
+        t = fabs(n);
 
+        if (s == 0 || t == 0) {
+            printf("Требуется ввести два ненулевых числа");
+        }
+        else {
+            printf("m + n = %.2f\nm - n = %.2f\nm * n = %.2f\nm / n = %.2f", s + t, s - t, s * t, s / t);
+        }
     }
     else {
         printf("Задания с таким номером нет, только 1-5");
     }
     return 0;
 }
-
-
