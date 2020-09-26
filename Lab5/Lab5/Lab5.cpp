e1b4be9583099113a39f9d21def9e6e9e431ea7a
София Кокорева <wisesoff@ya.ru>
2020-09-26 14:57:02 +0300

    lab5.1

---
+++ Lab5/Lab5/Lab5.cpp
@@ -0,0 +1,24 @@
+﻿#include <stdio.h>
+#include <math.h>
+#include <locale.h>
+
+int main()
+{
+    setlocale(LC_ALL, "Rus");
+
+    float x1, x2, y1, y2;
+
+    printf("Введите координаты точки А:\nx1 = ");
+    scanf_s("%f", &x1);
+    printf("y1 = ");
+    scanf_s("%f", &y1);
+
+    printf("Введите координаты точки B:\nx2 = ");
+    scanf_s("%f", &x2);
+    printf("y2 = ");
+    scanf_s("%f", &y2);
+
+    printf("Расстояние между точками А и В = %.2f", sqrt(pow(x2 - x1, 2) + pow(y2 - y1, 2)));
+    
+    return 0;
+}
