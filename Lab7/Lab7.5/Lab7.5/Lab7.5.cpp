996cc8f282933faedf2f1780780d03f72a5c342c
София Кокорева <wisesoff@ya.ru>
2020-11-01 22:11:27 +0300

    готово

---
+++ Lab7/Lab7.5/Lab7.5/Lab7.5.cpp
@@ -0,0 +1,16 @@
+﻿#include <stdio.h>
+#include <math.h>
+
+int main()
+{
+	float a, b, x;
+	printf("A = ");
+	scanf_s("%d", &a);
+	printf("B = ");
+	scanf_s("%d", &b);
+	x = -(b / a);
+	printf("x = %f", x);
+
+	return 0;
+}
+
