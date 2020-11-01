f822b440076983a5b11b884089c2076c67358214
София Кокорева <wisesoff@ya.ru>
2020-11-01 16:34:14 +0300

    готово

---
+++ Lab6/Lab6.2/Lab6.2/Lab6.2.cpp
@@ -0,0 +1,26 @@
+﻿#include <stdio.h>
+#include <math.h>
+#include <locale.h>
+
+int main()
+{
+    setlocale(LC_ALL, "Rus");
+
+    int A, B, C;
+    printf("A = ");
+    scanf_s("%d", &A);
+    printf("B = ");
+    scanf_s("%d", &B);
+    printf("C = ");
+    scanf_s("%d", &C);
+
+    int a = A;
+    int b = B;
+    A = b;
+    B = C;
+    C = a;
+
+    printf("Новые значения:\nA = %d\nB = %d\nC = %d", A, B, C);
+
+    return 0;
+}
