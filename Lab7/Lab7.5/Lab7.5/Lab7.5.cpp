be528fa95ac95344109df5683fbb189beff58e75
София Кокорева <wisesoff@ya.ru>
2020-11-01 22:28:26 +0300

    готово

--- Lab7/Lab7.5/Lab7.5/Lab7.5.cpp
+++ Lab7/Lab7.5/Lab7.5/Lab7.5.cpp
@@ -1,16 +1,22 @@
 ﻿#include <stdio.h>
 #include <math.h>
+#include <locale.h>
 
 int main()
 {
-	float a, b, x;
+	setlocale(LC_ALL, "Rus");
+	
+	int a, b;
 	printf("A = ");
 	scanf_s("%d", &a);
 	printf("B = ");
 	scanf_s("%d", &b);
-	x = -(b / a);
-	printf("x = %f", x);
-
+	if (a != 0) {
+		printf("x = %f", (float) (- b) / a);
+	}
+	else {
+		printf("А не должно быть 0");
+	}
 	return 0;
 }
 
---
+++ Lab7/Lab7.6/Lab7.6/Lab7.6.cpp
@@ -0,0 +1,23 @@
+﻿#include <stdio.h>
+#include <math.h>
+
+int main()
+{
+	float a1, b1, a2, b2, c1, c2, x;
+	printf("a1 = ");
+	scanf_s("%d", &a1);
+	printf("b1 = ");
+	scanf_s("%d", &b1);
+	printf("c1 = ");
+	scanf_s("%d", &c1);
+	printf("a2 = ");
+	scanf_s("%d", &a2);
+	printf("b2 = ");
+	scanf_s("%d", &b2);
+	printf("c2 = ");
+	scanf_s("%d", &c2);
+	x = ;
+	printf("x = %f", x);
+
+	return 0;
+}
