c656487c81f5e70fd9e8d1c3096cdefe6680c034
София Кокорева <wisesoff@ya.ru>
2020-11-01 20:44:15 +0300

    готово

---
+++ Lab7/Lab7.4/Lab7.4/Lab7.4.cpp
@@ -0,0 +1,22 @@
+﻿#include <stdio.h>
+#include <math.h>
+#include <locale.h>
+
+int main()
+{
+	setlocale(LC_ALL, "Rus");
+
+	int v1, v2, s, t;
+	printf("Скорость первого автомобиля (км/ч): ");
+	scanf_s("%d", &v1);
+	printf("Скорость второго автомобиля (км/ч): ");
+	scanf_s("%d", &v2);
+	printf("Расстояние между автомобилями (км): ");
+	scanf_s("%d", &s);
+	printf("Сколько часов они едут: ");
+	scanf_s("%d", &t);
+	printf("Расстояние между автомобилями спустя %dч = %d", t, s + v1 * t + v2 * t);
+
+	return 0;
+}
+
