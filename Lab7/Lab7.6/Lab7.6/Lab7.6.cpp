e268bfc78b9d5ce9340d9c3a380134a76721f174
София Кокорева <wisesoff@ya.ru>
2020-11-01 23:38:15 +0300

    готово

--- Lab7/Lab7.6/Lab7.6/Lab7.6.cpp
+++ Lab7/Lab7.6/Lab7.6/Lab7.6.cpp
@@ -1,9 +1,13 @@
 ﻿#include <stdio.h>
 #include <math.h>
+#include <locale.h>
 
 int main()
 {
-	float a1, b1, a2, b2, c1, c2, x;
+	setlocale(LC_ALL, "Rus");
+
+	int a1, b1, a2, b2, c1, c2, d;
+	float x, y;
 	printf("a1 = ");
 	scanf_s("%d", &a1);
 	printf("b1 = ");
@@ -16,8 +20,16 @@ int main()
 	scanf_s("%d", &b2);
 	printf("c2 = ");
 	scanf_s("%d", &c2);
-	x = ;
-	printf("x = %f", x);
 
+	d = a1 * b2 - a2 * b1;
+	if (d == 0) {
+		printf("Решений бесконечно много или их нет");
+	}
+	else {
+		x = (c1 * b2 - c2 * b1) / d;
+		y = (c2 * a1 - c1 * a2) / d;
+
+		printf("x = %f, y = %f", x, y);
+	}
 	return 0;
 }
