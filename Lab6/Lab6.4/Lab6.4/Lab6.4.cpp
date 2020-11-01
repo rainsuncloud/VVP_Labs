1da149088917e590ef6de96a13265145e83d1cc2
София Кокорева <wisesoff@ya.ru>
2020-11-01 17:37:01 +0300

    готово

---
+++ Lab6/Lab6.4/Lab6.4/Lab6.4.cpp
@@ -0,0 +1,16 @@
+﻿#include <stdio.h>
+#include <math.h>
+#include <locale.h>
+
+int main()
+{
+    setlocale(LC_ALL, "Rus");
+
+    int x, y;
+    printf("x = ");
+    scanf_s("%d", &x);
+    y = 3 * pow(x, 6) - 6 * pow(x, 2) - 7;
+    printf("y = %d", y);
+   
+    return 0;
+}
