74b4f18a9cc6868deaac366c29a6e0a60dd1961f
София Кокорева <wisesoff@ya.ru>
2020-11-01 19:07:01 +0300

    готово

---
+++ Lab7/Lab7.1/Lab7.1/Lab7.1.cpp
@@ -0,0 +1,20 @@
+﻿#include <stdio.h>
+#include <math.h>
+#include <locale.h>
+
+int main()
+{
+    setlocale(LC_ALL, "Rus");
+
+    float a;
+    printf("Угол a = ");
+    scanf_s("%f", &a);
+
+    if ((a >= 0) && (a <= 360)) {
+        printf("Значение угла а в радианах = %.2f pi", a / 180);
+    }
+    else {
+        printf("Требуется ввести значение угла от 0 до 360");
+    }
+    return 0;
+}
