aa13e506c83380d713d546aeb7f96a5fa9ee3f85
София Кокорева <wisesoff@ya.ru>
2020-11-01 18:02:08 +0300

    готово

---
+++ Lab6/Lab6.6/Lab6.6/Lab6.6.cpp
@@ -0,0 +1,20 @@
+﻿#include <stdio.h>
+#include <math.h>
+#include <locale.h>
+
+int main()
+{
+    setlocale(LC_ALL, "Rus");
+
+    int A, a;
+    printf("A = ");
+    scanf_s("%d", &A);
+
+    a = A * A;
+    A = a * a;
+    a = A * A;
+
+    printf("A^8 = %d", a);
+
+    return 0;
+}
