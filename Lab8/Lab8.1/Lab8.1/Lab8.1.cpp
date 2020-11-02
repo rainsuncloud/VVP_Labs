1fb940a9b106d1230d594fd0ef47df05baa2c4c1
София Кокорева <wisesoff@ya.ru>
2020-11-02 19:51:38 +0300

    готово

---
+++ Lab8/Lab8.1/Lab8.1/Lab8.1.cpp
@@ -0,0 +1,15 @@
+﻿#include <stdio.h>
+#include <locale.h>
+
+int main()
+{
+    setlocale(LC_ALL, "Rus");
+
+    int a;
+    printf("Размер файла (байт): ");
+    scanf_s("%d", &a);
+    printf("Размер файла (Кбит): %d", a / 1024);
+    
+    return 0;
+}
+
