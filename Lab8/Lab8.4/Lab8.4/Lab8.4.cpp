70520d31dbc8be815efe10b3cc434e7db465a563
София Кокорева <wisesoff@ya.ru>
2020-11-02 20:27:16 +0300

    готово

---
+++ Lab8/Lab8.4/Lab8.4/Lab8.4.cpp
@@ -0,0 +1,22 @@
+﻿#include <stdio.h>
+#include <locale.h>
+#include <math.h>
+
+int main()
+{
+    setlocale(LC_ALL, "Rus");
+
+    int A, B;
+    printf("Введите двузначное число: ");
+    scanf_s("%d", &A);
+    if (((A >= 10) && (A < 100)) || ((A <= -10) && (A > -100))) {
+        B = A / 10;
+        A = A - B * 10;
+        A = A * 10 + B;
+        printf("При переставлении цифр местами получено число %d", A);
+    }
+    else {
+        printf("Необходимо  ввести двузначное число");
+    }
+    return 0;
+}
