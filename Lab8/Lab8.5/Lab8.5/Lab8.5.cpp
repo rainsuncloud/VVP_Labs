95810a4ba6447dae48acd22dc92f563cab9f75df
София Кокорева <wisesoff@ya.ru>
2020-11-02 20:42:53 +0300

    готово

---
+++ Lab8/Lab8.5/Lab8.5/Lab8.5.cpp
@@ -0,0 +1,21 @@
+﻿#include <stdio.h>
+#include <locale.h>
+#include <math.h>
+
+int main()
+{
+    setlocale(LC_ALL, "Rus");
+
+    int A, B;
+    printf("Введите трехзначное число: ");
+    scanf_s("%d", &A);
+    if (((A >= 100) && (A < 1000)) || ((A <= -100) && (A > -1000))) {
+        B = A / 100;
+        A = (A - B * 100) * 10 + B;
+        printf("При перемещении первой цифры назад получено число %d", A);
+    }
+    else {
+        printf("Необходимо  ввести трехзначное число");
+    }
+    return 0;
+}
