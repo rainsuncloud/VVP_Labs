3066f8d5dc4628c5cfc0272aeb69352b3ed97bc6
София Кокорева <wisesoff@ya.ru>
2020-11-02 20:05:49 +0300

    готово

---
+++ Lab8/Lab8.2/Lab8.2/Lab8.2.cpp
@@ -0,0 +1,21 @@
+﻿#include <stdio.h>
+#include <locale.h>
+#include <math.h>
+
+int main()
+{
+    setlocale(LC_ALL, "Rus");
+
+    int A, B;
+    printf("Введите целые положительные числа, такие, что А > B\nA = ");
+    scanf_s("%d", &A);
+    printf("B = ");
+    scanf_s("%d", &B);
+    if ((A > B) && (A > 0) && (B > 0)) {
+        printf("На отрезке длины %d размещено %d отрезков длины %d", A, A / B, B);
+    }
+    else {
+        printf("Необходимо  ввести целые положительные числа, при этом A должно быть больше B");
+    }
+    return 0;
+}
