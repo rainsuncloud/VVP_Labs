6c8c44ad115214ac16ca7ba8707a306405792b49
София Кокорева <wisesoff@ya.ru>
2020-11-03 18:10:35 +0300

    готово

---
+++ Lab9/Lab9.1/Lab9.1/Lab9.1.cpp
@@ -0,0 +1,14 @@
+﻿#include <stdio.h>
+#include <locale.h>
+
+int main()
+{
+    setlocale(LC_ALL, "Rus");
+
+    int a;
+    printf("Сколько секунд прошло с начала суток: ");
+    scanf_s("%d", &a);
+    printf("С начала последней минуты прошло %d секунд", a % 60);
+
+    return 0;
+}
