985b84c356b7dcdb1def0d3cc309c839c5ac6716
София Кокорева <wisesoff@ya.ru>
2020-12-22 20:11:26 +0300

    vse

---
+++ Lab11/Lab11.5/Lab11.5/Lab11.5.cpp
@@ -0,0 +1,25 @@
+﻿#include <stdio.h>
+#include <locale.h>
+#include <math.h>
+
+int main()
+{
+    setlocale(LC_ALL, "Rus");
+
+    int x;
+    printf("x = ");
+    scanf_s("%d", &x);
+
+    if ((x > 0) && (x % 2 == 0))
+        printf_s("Положительное четное число");
+    else if (x > 0)
+        printf_s("Положительное нечетное число");
+    else if ((x < 0) && (x % 2 == 0))
+        printf_s("Отрицательное четное число");
+    else if (x < 0)
+        printf_s("Отрицательное нечетное число");
+    else
+        printf_s("Нулевое число");
+
+    return 0;
+}
