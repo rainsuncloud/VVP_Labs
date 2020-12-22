ac28345f2ef7903e389f4794adae198c29cfd9f5
София Кокорева <wisesoff@ya.ru>
2020-12-22 20:27:00 +0300

    готово

---
+++ Lab11/Lab11.6/Lab11.6/Lab11.6.cpp
@@ -0,0 +1,31 @@
+﻿#include <stdio.h>
+#include <locale.h>
+#include <math.h>
+
+int main()
+{
+    setlocale(LC_ALL, "Rus");
+
+    int x;
+    printf("x = ");
+    scanf_s("%d", &x);
+
+    if ((x >= 1) && (x < 1000)) {
+       if ((x % 2 == 0) && (x < 10))
+            printf_s("Однозначное четное число");
+       else if (x < 10)
+            printf_s("Однозначное нечетное число");
+       else if ((x < 100) && (x % 2 == 0))
+            printf_s("Двузначное четное число");
+       else if (x < 100)
+            printf_s("Двузначное нечетное число");
+       else if (x % 2 == 0)
+           printf_s("Трехзначное четное число");
+       else 
+           printf_s("Трехзначное нечетное число");
+    }
+    else
+        printf_s("Требуется ввести число в диапазоне 1 - 999");
+
+    return 0;
+}
