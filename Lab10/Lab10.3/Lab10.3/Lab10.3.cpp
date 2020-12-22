5c4196bcf714a909409a3a2435bc2ddb92d8cc29
София Кокорева <wisesoff@ya.ru>
2020-12-22 17:46:41 +0300

    sdelano

---
+++ Lab10/Lab10.3/Lab10.3/Lab10.3.cpp
@@ -0,0 +1,18 @@
+﻿#include <stdio.h>
+#include <locale.h>
+
+int main()
+{
+    setlocale(LC_ALL, "Rus");
+
+    int a;
+    printf("a = ");
+    scanf_s("%d", &a);
+    if ((a % 2 == 0) && (a > 9) && (a < 100)) {
+        printf_s("Высказывание «Данное число является четным двузначным» истинно");
+    }
+    else
+        printf_s("Высказывание «Данное число является четным двузначным» ложно");
+
+    return 0;
+}
