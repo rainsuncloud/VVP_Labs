8fc0bd839c85ef3f794c6e5fd7b27c1d8fea2dd4
София Кокорева <wisesoff@ya.ru>
2020-12-22 17:42:13 +0300

    ok

---
+++ Lab10/Lab10.2/Lab10.2/Lab10.2.cpp
@@ -0,0 +1,22 @@
+﻿#include <stdio.h>
+#include <locale.h>
+
+int main()
+{
+    setlocale(LC_ALL, "Rus");
+
+    int a, b, c;
+    printf("a = ");
+    scanf_s("%d", &a);
+    printf("b = ");
+    scanf_s("%d", &b);
+    printf("c = ");
+    scanf_s("%d", &c);
+    if ((a < b) && (b < c)) {
+        printf_s("Высказывание «Справедливо двойное неравенство A < B < C» истинно");
+    }
+    else
+        printf_s("Высказывание «Справедливо двойное неравенство A < B < C» ложно");
+
+    return 0;
+}
