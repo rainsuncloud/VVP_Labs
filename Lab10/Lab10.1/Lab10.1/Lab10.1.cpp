f7096e7a772bc9a3ad2b9f4ee98728648e318ab2
София Кокорева <wisesoff@ya.ru>
2020-12-22 17:38:50 +0300

    ujnjd

---
+++ Lab10/Lab10.1/Lab10.1/Lab10.1.cpp
@@ -0,0 +1,20 @@
+﻿#include <stdio.h>
+#include <locale.h>
+
+int main()
+{
+    setlocale(LC_ALL, "Rus");
+
+    int a, b;
+    printf("a = ");
+    scanf_s("%d", &a);
+    printf("b = ");
+    scanf_s("%d", &b);
+    if ((a > 2) && (b <= 3)) {
+        printf_s("Высказывание истинно");
+    }
+    else
+        printf_s("Высказывание ложно");
+
+    return 0;
+}
