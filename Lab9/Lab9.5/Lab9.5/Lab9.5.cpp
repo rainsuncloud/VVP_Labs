60610066477588abe02d96359cd39d8e06f4e199
София Кокорева <wisesoff@ya.ru>
2020-12-22 16:13:59 +0300

    готов

---
+++ Lab9/Lab9.5/Lab9.5/Lab9.5.cpp
@@ -0,0 +1,20 @@
+﻿#include <stdio.h>
+#include <locale.h>
+
+int main()
+{
+	setlocale(LC_ALL, "Rus");
+	int a;
+
+	printf_s("Введите год \n");
+	scanf_s("%d", &a);
+
+	if (a > 0)
+	{
+		printf_s("%d год относится к %d столетию", a, (a - 1) / 100 + 1);
+	}
+	else
+		printf_s("Требуется ввести целое положительное число");
+
+	return 0;
+}
