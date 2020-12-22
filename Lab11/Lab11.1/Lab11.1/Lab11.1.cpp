5fa218c006681d3154dbafaa9667ba450e533758
София Кокорева <wisesoff@ya.ru>
2020-12-22 19:27:10 +0300

    vse

---
+++ Lab11/Lab11.1/Lab11.1/Lab11.1.cpp
@@ -0,0 +1,29 @@
+﻿#include <stdio.h>
+#include <locale.h>
+#include <math.h>
+
+int main()
+{
+    setlocale(LC_ALL, "Rus");
+
+    int a, b;
+    printf("a = ");
+    scanf_s("%d", &a);
+    printf("b = ");
+    scanf_s("%d", &b);
+
+    if ((a != b) && (a > b)) {
+        b = a;
+        printf_s("a = %d\nb = %d", a, b);
+    }
+    else if ((a != b) && (a < b)) {
+        a = b;
+        printf_s("a = %d\nb = %d", a, b);
+    }
+    else {
+        a = 0;
+        b = 0;
+        printf_s("a = %d\nb = %d", a, b);
+    }
+    return 0;
+}
