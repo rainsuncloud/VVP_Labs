3afe0ef610d2e5207a94ebe897b1f6719a054ddf
София Кокорева <wisesoff@ya.ru>
2020-12-22 14:28:30 +0300

    готово

---
+++ Lab9/Lab9.1/Lab9.2/Lab9.2/Lab9.2/Lab9.2.cpp
@@ -0,0 +1,20 @@
+﻿#include <stdio.h>
+#include <locale.h>
+
+int main()
+{
+	setlocale(LC_ALL, "Rus");
+	int k;
+
+	printf_s("Сколько дней прошло с начала года?\n");
+	scanf_s("%d", &k);
+
+	if ((k > 0) && (k < 366))
+	{
+		printf_s("Сегодня %d день недели (0 - воскресенье, ..., 6 - суббота)", k % 7);
+	}
+	else
+		printf_s("Требуется ввести число в диапазоне 1 - 365");
+
+	return 0;
+}
