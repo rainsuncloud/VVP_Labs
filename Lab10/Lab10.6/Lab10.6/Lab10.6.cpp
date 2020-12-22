a1a4acfd4f92666213766ca870491f536e4c17f6
София Кокорева <wisesoff@ya.ru>
2020-12-22 18:31:58 +0300

    ok

---
+++ Lab10/Lab10.6/Lab10.6/Lab10.6.cpp
@@ -0,0 +1,31 @@
+﻿#include <stdio.h>
+#include <locale.h>
+#include <math.h>
+
+int main()
+{
+    setlocale(LC_ALL, "Rus");
+
+    int a, b, c;
+    printf("a = ");
+    scanf_s("%d", &a);
+    printf("b = ");
+    scanf_s("%d", &b);
+    printf("c = ");
+    scanf_s("%d", &c);
+
+    if ((a > 0) && (b > 0) && (c > 0)) {
+        a = a * a;
+        b = b * b;
+        c = c * c;
+
+        if ((a + b == c) || (a + c == b) || (b + c == a)) {
+            printf_s("Высказывание «Треугольник со сторонами a, b, c является прямоугольным» истинно");
+        }
+        else
+            printf_s("Высказывание «Треугольник со сторонами a, b, c является прямоугольным» ложно");
+    }
+    else
+        printf_s("Требуется ввести положительные числа");
+    return 0;
+}
